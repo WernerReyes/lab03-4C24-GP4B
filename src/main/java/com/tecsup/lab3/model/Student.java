24e37fa5dc49e34521896ceacd8d29f93f8d1ae6
mac03 <mac03@localhost>
2024-03-26 15:22:47 -0500

    Secon Commit

--- src/main/java/com/tecsup/lab3/model/Student.java
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -3,8 +3,9 @@ package com.tecsup.lab3.model;
 public class Student {
 
 	public static void main(String[] args) {
+		
 		String nombre = "ERMINIO";
-		System.out.print("Hola mundo con MAVEN Y GIT CON: "+ nombre);
+		System.out.print("Hola mundo en MAVEN Y GIT CON: "+ nombre);
 
 	}
 

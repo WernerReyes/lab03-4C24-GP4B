89b14617ea0cf5ee9fa4e84e78867639d9ae7769
mac03 <mac03@localhost>
2024-03-26 15:20:37 -0500

    Commit con Erminio

---
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -0,0 +1,11 @@
+package com.tecsup.lab3.model;
+
+public class Student {
+
+	public static void main(String[] args) {
+		String nombre = "ERMINIO";
+		System.out.print("Hola mundo con MAVEN Y GIT CON: "+ nombre);
+
+	}
+
+}

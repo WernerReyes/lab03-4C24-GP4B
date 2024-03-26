1d5a36e9e500760ccaa1c252d8d387e148c4fa2d
WernerReyes <werner.reyes@tecsup.edu.pe>
2024-03-26 15:22:40 -0500

    New field added

---
+++ src/main/java/com/tecsup/lab03/lab03_4C24_GP4B/StudentDto.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab03.lab03_4C24_GP4B;
+
+public class StudentDto {
+
+	public static void main(String[] args) {
+		System.out.println("Created by Werner Reyes Pilco");
+
+	}
+
+}

a25f1ef5276f74a31453664124ef261eccb1abc1
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2024-03-26 20:20:25 -0500

    commit desde class StudentReport

---
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab3.view;
+
+public class StudentReport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Rolando Casapcion");
+	}
+
+}

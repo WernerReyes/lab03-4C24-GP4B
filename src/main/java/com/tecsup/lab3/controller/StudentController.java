67262ce99df55ff27388b183c793037bb3594017
Erminio Guerrero <https://github.com/PAREDESERMINIO/Prubas.git>
2024-03-26 22:33:28 -0500

    "Subiendo Cambios Modificados en el Spring 2"

---
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab3.controller;
+
+import java.util.Scanner;
+
+public class StudentController {
+	public static void main(String[] args) {
+        int numero1, numero2, suma;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduzca el primer número: ");
+        numero1 = teclado.nextInt();
+
+        System.out.print("Introduzca el segundo número: ");
+        numero2 = teclado.nextInt();
+
+        suma = numero1 + numero2;
+
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
